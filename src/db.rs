b725fb5838f3d1f0e0282a0083d375083f74fafb
chenjing17 <jing.chen.2021@scis.smu.edu.sg>
2024-03-20 13:43:57 +0800

    Update db.rs

--- src/db.rs
+++ src/db.rs
@@ -1,7 +1,7 @@
 use tokio_postgres::{NoTls, Config};
 
 pub async fn connect_to_database() -> tokio_postgres::Client {
-    let config_str = "postgres://postgres.wovuoddicfjlfzrysyuy:5%tu82@cFmH8KH*@aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres";
+    let config_str = "";
     
     let (client, connection) = Config::from_str(config_str, NoTls)
         .connect()
